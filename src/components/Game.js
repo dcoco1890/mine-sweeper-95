3692c61f9c7500e84a4290863d6ae3e52a49c1f9
Cohen <dcoco91@gmail.com>
2020-02-04 15:56:30 -0500

    added more to the onclick function called by play button. It now sets up the initial board data, and then logs it to conosle. because of the useeffect it will log to console anytime the data changes

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,44 +2,64 @@ import React, { useState, useEffect } from "react";
 import Board from "./Board";
 import Button from "./Button";
 
+// Create cell data, takes in height, width, and num of mines
+const createCellData = (h, w, m) => {
+  let cellArray = [];
+  for (let i = 0; i < h; i++) {
+    cellArray.push([]);
+    for (let j = 0; j < w; j++) {
+      cellArray[i][j] = {
+        x: i,
+        y: j,
+        isMine: false,
+        isFlag: false,
+        isRevealed: false,
+        isEmpty: false,
+        minesTouching: 0
+      };
+    }
+  }
+  // something here that sets up mines
+  // something here that figures out how many mines each square is touching
+
+  return cellArray;
+};
+
 const Game = () => {
+  // height and width
   const [width, setWidth] = useState(5);
   const [height, setHeight] = useState(5);
-  const [cells, makeCells] = useState(null);
+
+  // cells and set cells. the useeffect causes it to log the cells after clicking play
+  const [cells, setCells] = useState([]);
+  useEffect(() => console.log(cells));
   const [mines, setMines] = useState(5);
   // Keeps track of whether user is playing, could also create game data
   const [playing, isPlaying] = useState(false);
-
-  function createCellData(h, w, m) {
-    let cellArray = [];
-    for (let i = 0; i < h; i++) {
-      cellArray.push([]);
-      for (let j = 0; j < w; j++) {
-        cellArray[i][j] = {
-          x: i,
-          y: j,
-          isMine: false,
-          isFlag: false,
-          isRevealed: false,
-          isEmpty: false,
-          minesTouching: 0
-        };
-      }
-    }
-    // something here that sets up mines
-    // something here that figures out how many mines each square is touching
-
-    return cellArray;
-  }
 
   return (
     <div className="App">
       {/* When not playing, display Play Button that sets isplaying to true. */}
       {!playing ? (
-        <Button text="Play" onClick={() => isPlaying(true)} />
+        <Button
+          text="Play"
+          onClick={() => {
+            isPlaying(true);
+            // create var to hold cell data, then set it in state with set cells
+            let dataArr = createCellData(height, width, mines);
+            setCells(dataArr);
+          }}
+        />
       ) : (
-        <Button text="Reset" onClick={() => isPlaying(false)} />
+        <Button
+          text="Reset"
+          onClick={() => {
+            isPlaying(false);
+          }}
+        />
       )}
+
+      {/*When playing is true, display the board with this info*/}
       {playing && <Board width={width} height={height} mines={mines} />}
     </div>
   );
