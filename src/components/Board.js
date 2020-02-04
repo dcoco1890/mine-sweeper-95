54a8f681bf7b995c6807aa1504bcc1b1c97533b8
Cohen <dcoco91@gmail.com>
2020-02-04 15:58:46 -0500

    removed the unneeded function on board comp

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,15 +1,6 @@
 import React from "react";
 import Cell from "../components/Cell";
 
-const makeBoard = (h, w) => {
-  for (let i = 0; i < h; i++) {
-    console.log(i);
-    for (let j = 0; j < w++; j++) {
-      return <Cell />;
-    }
-  }
-};
-
 const Board = props => {
   const { width, height, mines } = props;
   console.log(width);
