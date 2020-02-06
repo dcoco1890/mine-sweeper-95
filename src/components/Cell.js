8a4033ad11c73272c59ceb7757ca04c056ead25d
Curtis Jochen <curtis.jochen@gmail.com>
2020-02-05 23:47:45 -0500

    render grid to screen. Set up a function for testing the click functionality. Also clears and resets the board when you click reset. I kept some of your code modified it and commeted out the stuff I thought we should get rid of/change. I didn't delete it though in case you wanted to go a differenet direction. Everything works as I intend it to.

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 const Cell = props => {
 
 
-  return <div className="cell"></div>;
+  return <div className={props.cellClass}
+  id={props.cellId}
+  onClick={() => props.clickTest(props.row, props.col)}></div>;
 };
 
 export default Cell;
