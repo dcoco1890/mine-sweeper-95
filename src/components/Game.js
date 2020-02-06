2946e27dc2b22881a41e1d2cbb71c82165c938b6
Cohen <dcoco91@gmail.com>
2020-02-06 12:58:35 -0500

    removed create cell func from the Game comp and moved it to helper functions area to clean it up. put rows and cols back in state, so it can be user configed later. because of these changes needed to modify some other functios

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,86 +1,59 @@
 import React, { useState, useEffect } from "react";
 import Board from "./Board";
 import Button from "./Button";
-import Helpfuncs from "../utils/help-func/index";
-
-// let numRow = 30;
-// let numCol = 50;
-
-// Create cell data, takes in height, width, and num of mines
-const createCellData = (r, c, m) => {
-    let cellArray = [];
-    for (let i = 0; i < r; i++) {
-        cellArray.push([]);
-        for (let j = 0; j < c; j++) {
-            cellArray[i][j] = {
-                x: i,
-                y: j,
-                isMine: false,
-                isFlag: false,
-                isRevealed: false,
-                isEmpty: false,
-                minesTouching: 0
-            };
-        }
-    }
-
-    // Works, sets random mines
-    cellArray = Helpfuncs.plantMines(r, c, m, cellArray);
-    // something here that figures out how many mines each square is touching
-
-    return cellArray;
-};
+// Help is just a function that gives us the cell data
+import Help from "../utils/help-func/index";
 
 const Game = () => {
-    const [rows, setRows] = useState(5);
-    const [cols, setCols] = useState(5);
-    const [mines, setMines] = useState(2);
+  const [rows, setRows] = useState(30);
+  const [cols, setCols] = useState(50);
+  const [mines, setMines] = useState(50);
 
-    // The Array of Cell Data
-    const [cells, setCells] = useState(() => {
-        return createCellData(rows, cols, mines);
-    });
-    // Keeps track of whether user is playing
-    const [playing, setPlaying] = useState(false);
-    useEffect(() => console.log(cells)); // This logs cell data to console
+  // The Array of Cell Data
+  const [cells, setCells] = useState(() => {
+    return Help.createCellData(rows, cols, mines);
+  });
+  // Keeps track of whether user is playing
+  const [playing, setPlaying] = useState(false);
+  useEffect(() => console.log(cells)); // This logs cell data to console
 
-    const clickTest = (row, col) => {
-        let copy = [...cells];
-        copy[row][col].isRevealed = true;
-        setCells(copy);
-    };
+  const clickTest = (row, col) => {
+    let copy = [...cells];
+    copy[row][col].isRevealed = true;
+    setCells(copy);
+  };
 
-    const resetBoard = () => {
-        setCells(createCellData(rows, cols, mines));
-    };
+  const resetBoard = () => {
+    setCells(createCellData(rows, cols, mines));
+  };
 
-    return (
-        <div className="App">
-            {/* When not playing, display Play Button that sets isplaying to true. */}
-            {!playing ? (
-                <Button
-                    text="Play"
-                    onClick={() => {
-                        setPlaying(true);
-                    }}
-                />
-            ) : (
-                    <Button
-                        text="Reset"
-                        onClick={() => {
-                            setPlaying(false);
-                            resetBoard();
-                        }}
-                    />
-                )}
+  return (
+    <div className="App">
+      {/* When not playing, display Play Button that sets isplaying to true. */}
+      {!playing ? (
+        <Button
+          text="Play"
+          onClick={() => {
+            setPlaying(true);
+          }}
+        />
+      ) : (
+        <Button
+          text="Reset"
+          onClick={() => {
+            setPlaying(false);
+            resetBoard();
+          }}
+        />
+      )}
 
-            {/*When playing is true, display the board with this info*/}
-            {/* {playing && <Board width={width} height={height} mines={mines} />} */}
-            {playing && (
-                <Board cells={cells} rows={rows} cols={cols} clickTest={clickTest} />
-            )}
-        </div>
-    );
+      {/*When playing is true, display the board with this info*/}
+      {/* {playing && <Board width={width} height={height} mines={mines} />} */}
+      {playing && (
+        <Board cells={cells} rows={rows} cols={cols} clickTest={clickTest} />
+      )}
+    </div>
+  );
 };
 
 export default Game;
