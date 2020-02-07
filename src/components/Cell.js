2a95dea3d7cd7a2fbb41645220e27780f71ec5a0
Cohen <dcoco91@gmail.com>
2020-02-06 20:13:23 -0500

    moved flag constant to cell. added bomb const. added function to display bomb when clicked if bomb exists

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
 
+const FLAG = "🚩";
+const BOMB = "💣";
+
+
 const Cell = props => {
+const { cellValue } = props;
+
+const [cellval, setCellVal] = useState(null);
+
+
+const getCellVal = (cellValue) => {
+  let x = '';
+  if(cellValue.isMine) {
+    x = BOMB;
+  }
+
+  if(cellValue.isFlag) {
+   x = FLAG;
+  }
+  return x
+}
   return (
     <div
       className={props.cellClass}
       id={props.cellId}
-      onClick={() => props.clickTest(props.row, props.col)}
+      onClick={() => {
+        props.clickTest(props.row, props.col);
+        const CV = getCellVal(cellValue);
+        if(CV){
+          setCellVal(CV)
+        }
+      }}
     >
-      {props.minesTouching}
+     {cellval}
     </div>
   );
 };
