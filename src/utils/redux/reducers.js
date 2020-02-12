c2fe387b50f2c20a5806de6fb37a03d06e519a6b
Cohen <dcoco91@gmail.com>
2020-02-12 10:48:12 -0500

    added some initial state

--- src/utils/redux/reducers.js
+++ src/utils/redux/reducers.js
@@ -1,16 +1,25 @@
 import { PLANT_FLAG, CLICK_CELL, CREATE_GAME, REVEAL_BOARD } from "./constants";
 import Help from "../help-func/index";
-// import { combineReducers } from "redux";
+import { combineReducers } from "redux";
+
+const initialState = {
+  gameData: {
+    rows: 10,
+    cols: 10,
+    mines: 20,
+    flagsPlanted: 0
+  },
+  // Hard Code Initial Values
+  gameArray: Help.createCellData(10, 10, 20)
+};
 
 const mineSweeping = (state = {}, action) => {
   //   console.log(state);
   switch (action.type) {
     case CREATE_GAME:
       return Object.assign({}, action.gameArray);
-      
+
     case CLICK_CELL:
-      //   console.log(action.payload.x);
-      //   console.log(action.payload.y);
       let copy = state;
       copy[action.payload.x][action.payload.y].isRevealed = true;
       if (copy[action.payload.x][action.payload.y].isEmpty) {
@@ -23,7 +32,7 @@ const mineSweeping = (state = {}, action) => {
           (copy[action.payload.x][action.payload.y].isRevealed = true)
         );
       }
-      
+
     case PLANT_FLAG:
       let newState = state;
       if (newState[action.payload.x][action.payload.y].isRevealed) {
